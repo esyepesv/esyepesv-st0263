a16285fabeaaae3d2554711510085ea4efe38424
root <root@DESKTOP-V1AO4B2.localdomain>
2023-03-04 20:48:41 -0500

    comunicacion gRPC

--- Reto2/apigateway/main.py
+++ Reto2/apigateway/main.py
@@ -1,35 +1,43 @@
-from flask import Flask, request, jsonify
 import os
+from flask import Flask, jsonify
+import grpc
+import sys
+sys.path.append('../service1-gRPC')
+import file_service_pb2
+import file_service_pb2_grpc
 
-def create_app():
-    app = Flask(__name__)
-    return app
 
-app = create_app()
+app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def get_users():
-    response = {'message': 'services: /find?file={filename} and /list'}
-    return jsonify(response), 200, {'Content-Type': 'application/json'}
+# crear un canal de comunicación con el servidor de gRPC
+channel = grpc.insecure_channel('localhost:50051')
 
-@app.route('/list', methods=['GET'])
+# crear un cliente de gRPC para el servicio de FileService
+file_service_client = file_service_pb2_grpc.FileServiceStub(channel)
+
+@app.route('/files', methods=['GET'])
 def list_files():
-    # obtener la lista de archivos en la carpeta 'files'
-    files = os.listdir('../files')
-    # devolver la lista de archivos como una respuesta JSON
-    return jsonify(files), 200
+    # crear una solicitud de gRPC para obtener la lista de archivos
+    request = file_service_pb2.ListFilesRequest()
+    # hacer la llamada al servicio de gRPC
+    response = file_service_client.ListFiles(request)
+    # convertir el campo "filenames" a una lista
+    filenames = list(response.filenames)
+    # devolver la lista de archivos como una respuesta HTTP
+    return jsonify(filenames)
 
-@app.route('/find', methods=['GET'])
-def find_file():
-    # obtener el parámetro de consulta 'file' de la solicitud
-    file = request.args.get('file')
-    # verificar si el archivo existe en la carpeta 'files'
-    if file in os.listdir('../files'):
-        # si el archivo existe, devolver una respuesta 200
-        return 'El archivo {} existe.'.format(file), 200
+
+@app.route('/find/<filename>', methods=['GET'])
+def file_exists(filename):
+    # crear una solicitud de gRPC para verificar si el archivo existe
+    request = file_service_pb2.FileExistsRequest(filename=filename)
+    # hacer la llamada al servicio de gRPC
+    response = file_service_client.FileExists(request)
+    # devolver la respuesta del servicio de gRPC como una respuesta HTTP
+    if response.exists:
+        return 'El archivo {} existe.'.format(filename), 200
     else:
-        # si el archivo no existe, devolver una respuesta 404
-        return 'El archivo {} no existe.'.format(file), 404
+        return 'El archivo {} no existe.'.format(filename), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
